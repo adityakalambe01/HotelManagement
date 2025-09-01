f543c1aed89d06e7eb25b57d01f8ac986f5002f5
AdityaAngular01 <aditya@angularminds.in>
2025-09-01 14:12:08 +0530

    feat(utils): export ApiError and utility functions (pick, paramsPicker, queryFilterOptionPicker)

---
+++ src/utils/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+    ApiError: require('./ApiError'),
+    pick: require('./pick'),
+    reqQueryFilterOptionsPicker: require("./queryFilterOptionPicker"),
+    paramsPicker: require("./paramsPicker")
+}
