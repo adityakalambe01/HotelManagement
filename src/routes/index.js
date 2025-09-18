5ecab02b52079d0bf12b80b8e5b7eafd416c813d
AdityaAngular01 <aditya@angularminds.in>
2025-09-18 18:39:03 +0530

    feat(middleware): integrate rate limiting and authentication into route setup

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,56 +8,66 @@ const emailVerificationRoutes = require("./emailVerification.routes");
 const hotelCategoryRoutes = require("./hotelCategory.routes");
 const hotelRoutes = require("./hotel.routes");
 const subscriptionPlanRoutes = require("./subscriptionPlan.routes");
+const {authLimiter, apiLimiter} = require("../middlewares/rateLimiter.middleware");
+const authenticate = require("../middlewares/authenticate.middleware");
 
 const defaultRoutes = [
     {
         path: "/auth",
-        route: authRoutes
+        route: authRoutes,
+        middlewares: [authLimiter]
     },
     {
         path: "/user",
-        route: userRoutes
+        route: userRoutes,
+        middlewares: [authenticate, apiLimiter]
     },
     {
         path: "/amenity-category",
-        route: amenityCategory
+        route: amenityCategory,
+        middlewares: [authenticate, apiLimiter]
     },
     {
         path: "/amenity",
-        route: amenityRoutes
+        route: amenityRoutes,
+        middlewares: [authenticate, apiLimiter]
     },
     {
         path: "/hotel-category",
-        route: hotelCategoryRoutes
+        route: hotelCategoryRoutes,
+        middlewares: [authenticate, apiLimiter]
     },
     {
         path: "/hotel",
-        route: hotelRoutes
+        route: hotelRoutes,
+        middlewares: [authenticate, apiLimiter]
     },
     {
         path: "/subscription-plans",
-        route: subscriptionPlanRoutes
+        route: subscriptionPlanRoutes,
+        middlewares: [authenticate, apiLimiter]
     },
     {
         path: "/",
-        route: emailVerificationRoutes
+        route: emailVerificationRoutes,
+        middlewares: [apiLimiter]
     },
 ]
 
 switch (config.env) {
     case 'development':
         defaultRoutes.forEach((route) => {
-            router.use(`/api/dev${route.path}`, route.route);
+            router.use(`/api/dev${route.path}`, ...route.middlewares, route.route);
         });
         break;
     case 'test':
         defaultRoutes.forEach((route) => {
-            router.use(`/api/test${route.path}`, route.route);
+            router.use(`/api/test${route.path}`, ...route.middlewares, route.route);
         });
         break;
     case 'production':
         defaultRoutes.forEach((route) => {
-            router.use(route.path, route.route);
+            router.use(route.path, ...route.middlewares, route.route);
         })
         break;
     default:
