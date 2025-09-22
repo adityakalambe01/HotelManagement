e36b7412659348a98b8b52bce7fccca8e8a38b98
AdityaAngular01 <aditya@angularminds.in>
2025-09-22 11:16:38 +0530

    feat(routes): add mainIndex constants for core API endpoints

---
+++ src/constant/routes.constant.js
@@ -0,0 +1,15 @@
+const mainIndex = {
+    AUTH: '/auth',
+    USER: '/user',
+    AMENITY_CATEGORY: '/amenity-category',
+    AMENITY: '/amenity',
+    HOTEL_CATEGORY: '/hotel-category',
+    HOTEL: '/hotel',
+    SUBSCRIPTION: '/subscription',
+    SUBSCRIPTION_PLANS: '/subscription-plans',
+    ROOM: '/room',
+    PAYMENT: '/payment',
+    EMAIL_VERIFICATION: '/',
+}
+
+module.exports = {mainIndex}
